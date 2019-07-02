24dcacdf22e557046b0f03db34cb91c6723a5849
js-tut <johnsmilga@gmail.com>
2019-07-02 14:17:00 -0700

    6-inline-css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
-
+// inline css
+// app css
+// css variables
+// file css
 function App() {
   return (
     <div>
       <h1>styled components</h1>
+      <h2 style={{ color: "red", textTransform: "uppercase" }}>
+        inline styles
+      </h2>
     </div>
   );
 }
