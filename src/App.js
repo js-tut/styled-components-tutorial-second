ec80e00be51b6246b74f7cb644525acdc6d1882d
js-tut <johnsmilga@gmail.com>
2019-07-02 16:06:47 -0700

    12-styled-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import "./App.css";
-import Title from "./components/Title";
-// inline css
-// app css
-// css variables
-// file css
+// const name = 'john'
+// const greeting = `hello my name is ${name}`
+// styled-components utilises tagged template literals to style your components
+
 function App() {
-  return (
-    <div>
-      <h1>styled components</h1>
-      <h2 style={{ color: "red", textTransform: "uppercase" }}>
-        inline styles
-      </h2>
-      <h2 className="global">global css</h2>
-      <h2 className="first">first</h2>
-      <h2 className="second">second</h2>
-      <Title />
-    </div>
-  );
+  return <div>hello from app</div>;
 }
 
 export default App;
