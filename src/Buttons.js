718c89b371658022bee51dbd51ff27e75a6c12ff
js-tut <johnsmilga@gmail.com>
2019-07-02 18:26:46 -0700

    15-variables

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+const color = "yellow";
+
 export const Button = styled.button`
   color: red;
   background: blue;
   text-transform: uppercase;
 `;
 export const SecondButton = styled.button`
-  color: blue;
-  background: red;
+  color: ${color};
+  background: var(--primaryColor);
   text-transform: uppercase;
 `;
