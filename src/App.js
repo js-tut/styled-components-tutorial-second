23b11bdba4feec7c92ae18886be7340ab65df980
js-tut <johnsmilga@gmail.com>
2019-07-02 17:16:53 -0700

    13-seperate-file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import "./App.css";
-import styled from "styled-components";
-// const name = 'john'
-// const greeting = `hello my name is ${name}`
-// styled-components utilises tagged template literals to style your components
-// function`` - tagged template literal
-const Button = styled.button`
-  color: red;
-  background: blue;
-  text-transform: uppercase;
-`;
-const SecondButton = styled.button`
-  color: blue;
-  background: red;
-  text-transform: uppercase;
-`;
-
+import { Button } from "./Buttons";
+import { SecondButton } from "./Buttons";
+import Header from "./components/Header";
 function App() {
   return (
     <div>
+      <Header />
       hello from app
       <Button>click me</Button>
       <SecondButton>click me i'm second</SecondButton>
---
+++ src/Buttons.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: red;
+  background: blue;
+  text-transform: uppercase;
+`;
+export const SecondButton = styled.button`
+  color: blue;
+  background: red;
+  text-transform: uppercase;
+`;
