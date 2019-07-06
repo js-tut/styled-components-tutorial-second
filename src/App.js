21f468b7713c6620e75a8ac45f6d24b423369110
js-tut <johnsmilga@gmail.com>
2019-07-06 14:17:01 -0700

    20-global-styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from "react";
 import "./App.css";
 import StyledHeader from "./components/StyledHeader";
 import PropsButton from "./components/PropsButton";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ * {
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+`;
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <StyledHeader title="hello from app.js" />
       <PropsButton color="blue">button number one</PropsButton>
       <PropsButton color="red">button number two</PropsButton>
