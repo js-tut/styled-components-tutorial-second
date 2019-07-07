a6da45e880894d3992f64af39b336149492bc606
js-tut <johnsmilga@gmail.com>
2019-07-06 17:43:18 -0700

    26-attrs-method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "./App.css";
-import Hero from "./components/Hero";
-import img1 from "./images/image-1.jpeg";
-import img2 from "./images/image-2.jpeg";
-import Banner from "./components/Banner";
-import { DangerButton } from "./examples/PropsButton";
+import { BasicInput, AwesomeInput } from "./components/Inputs";
 function App() {
   return (
     <div>
-      <Hero img={img1}>
-        <Banner title="this is my title" color="red">
-          <DangerButton big>click me</DangerButton>
-        </Banner>
-      </Hero>
-      <Hero img={img2} />
-      <Hero big />
+      <BasicInput />
+      <AwesomeInput />
+      <AwesomeInput type="email" placeholder="please enter email" />
     </div>
   );
 }
