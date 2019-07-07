b3b022531cfd1c00949ac38f35b380493e8f6eed
js-tut <johnsmilga@gmail.com>
2019-07-06 17:03:46 -0700

    25-banner-component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./App.css";
 import Hero from "./components/Hero";
 import img1 from "./images/image-1.jpeg";
 import img2 from "./images/image-2.jpeg";
+import Banner from "./components/Banner";
+import { DangerButton } from "./examples/PropsButton";
 function App() {
   return (
     <div>
-      <Hero img={img1} />
+      <Hero img={img1}>
+        <Banner title="this is my title" color="red">
+          <DangerButton big>click me</DangerButton>
+        </Banner>
+      </Hero>
       <Hero img={img2} />
       <Hero big />
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,9 @@ const Hero = styled.div`
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Hero;
