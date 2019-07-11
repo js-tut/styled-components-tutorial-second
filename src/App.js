f332f55505175ad8713981baeac73e96508afcc7
js-tut <johnsmilga@gmail.com>
2019-07-11 12:56:37 -0700

    29-theming

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { RedBox, GreenBox } from "./components/Boxes";
-import { Box } from "./components/AnimatedBox";
+import { ThemeProvider } from "styled-components";
+import { redTheme, greenTheme, blueTheme } from "./components/themes";
+import Navbar from "./components/Navbar";
+import Button from "./components/Button";
+import Content from "./components/Content";
 function App() {
+  const [theme, setTheme] = useState(redTheme);
+
+  const handleTheme = () => {
+    if (theme.primaryColor === "red") {
+      setTheme(greenTheme);
+    } else if (theme.primaryColor === "green") {
+      setTheme(blueTheme);
+    } else {
+      setTheme(redTheme);
+    }
+  };
   return (
-    <div>
-      <RedBox color="red" />
-      <GreenBox color="green" />
-      <Box />
-    </div>
+    <ThemeProvider theme={theme}>
+      <main>
+        <Navbar>
+          <Button onClick={handleTheme}>toggle theme</Button>
+        </Navbar>
+        <Content />
+      </main>
+    </ThemeProvider>
   );
 }
 
