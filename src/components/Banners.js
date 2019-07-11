9c59f392972c643f8687f261ec600a4f5b2738fe
js-tut <johnsmilga@gmail.com>
2019-07-11 14:46:44 -0700

    31-media-with-object

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
+const screen = {
+  phone: (...args) => {
+    const styles = args;
+    console.log(styles);
+
+    return `@media (min-width:576px){
+      ${styles};
+    }`;
+  }
+};
+
 export const Banner = styled.div`
   background: red;
   h1 {
     color: white;
     text-transform: uppercase;
   }
-  @media (min-width: 676px) {
+  ${screen.phone`background:blue;h1{
+    color:yellow
+  }`} /* @media (min-width: 676px) {
     background: blue;
     h1 {
       color: yellow;
     }
-  }
+  } */
 `;
 export const SecondBanner = styled.div`
   background: blue;
@@ -19,10 +32,12 @@ export const SecondBanner = styled.div`
     color: red;
     text-transform: uppercase;
   }
-  @media (min-width: 576px) {
+  ${screen.phone`background:green;h1{
+    color:blue
+  }`} /* @media (min-width: 576px) {
     background: green;
     h1 {
       color: blue;
     }
-  }
+  } */
 `;
